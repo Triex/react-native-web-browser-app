9ac0d0f5d3901687123fd781b4dd8de0a4b4cd7d
Alex Zarov <alex@imakewebsites.co>
2022-03-03 16:57:38 +1100

    Browserviewcontroller

--- src/browser/BrowserViewController.tsx
+++ src/browser/BrowserViewController.tsx
@@ -1,22 +1,25 @@
-import * as React from "react";
-import { Dimensions, View } from "react-native";
-import Animated from "react-native-reanimated";
-import { connect } from "react-redux";
-import { BrowserConfig, defaultConfig } from "./browserConfig";
-import { WholeStoreState } from "../store/store";
-import { isPortrait, updateOrientation } from "../store/uiState";
-import { DRAG_END_INITIAL } from "./bothBars/barSpring";
-import { FooterConnected } from "./footer/Footer";
-import { RetractibleHeaderConnected } from "./header/RetractibleHeader";
-import { DEFAULT_HEADER_RETRACTED_HEIGHT, DEFAULT_HEADER_REVEALED_HEIGHT } from "./header/TabLocationView";
-import { DefaultBarAwareWebView } from "./webView/BarAwareWebView";
-import { WebViewBackdrop } from "./webView/WebViewBackdrop";
+import * as React from 'react';
+import {Dimensions, View} from 'react-native';
+import Animated from 'react-native-reanimated';
+import {connect} from 'react-redux';
+import {BrowserConfig, defaultConfig} from './browserConfig.tsx';
+import {WholeStoreState} from '../store/store';
+import {isPortrait, updateOrientation} from '../store/uiState';
+import {DRAG_END_INITIAL} from './bothBars/barSpring';
+import {FooterConnected} from './footer/Footer';
+import {RetractibleHeaderConnected} from './header/RetractibleHeader';
+import {
+  DEFAULT_HEADER_RETRACTED_HEIGHT,
+  DEFAULT_HEADER_REVEALED_HEIGHT,
+} from './header/TabLocationView';
+import {DefaultBarAwareWebView} from './webView/BarAwareWebView';
+import {WebViewBackdrop} from './webView/WebViewBackdrop';
 
 const BrowserViewControllerUX = {
-    ShowHeaderTapAreaHeight: 0,
-    BookmarkStarAnimationDuration: 0.5,
-    BookmarkStarAnimationOffset: 80,
-}
+  ShowHeaderTapAreaHeight: 0,
+  BookmarkStarAnimationDuration: 0.5,
+  BookmarkStarAnimationOffset: 80,
+};
 
 // // https://github.com/cliqz/user-agent-ios/blob/develop/Client/Frontend/Browser/BrowserViewController.swift#L70
 // class AlertStackView extends React.Component<ViewProps, {}> {
@@ -57,112 +60,108 @@ const BrowserViewControllerUX = {
 // }
 
 interface Props {
-    config?: BrowserConfig,
-    updateOrientation: typeof updateOrientation,
+  config?: BrowserConfig;
+  updateOrientation: typeof updateOrientation;
 }
 
-interface State {
-
-}
+interface State {}
 
 export class BrowserViewController extends React.Component<Props, State> {
-    private readonly scrollY;
-    private readonly scrollEndDragVelocity = new Animated.Value(DRAG_END_INITIAL);
+  private readonly scrollY;
+  private readonly scrollEndDragVelocity = new Animated.Value(DRAG_END_INITIAL);
 
-    constructor(props: Props){
-        super(props);
+  constructor(props: Props) {
+    super(props);
 
-        const { config = defaultConfig, } = props;
-        const {
-            HEADER_RETRACTED_HEIGHT = DEFAULT_HEADER_RETRACTED_HEIGHT,
-            HEADER_REVEALED_HEIGHT = DEFAULT_HEADER_REVEALED_HEIGHT,
-        } = config.header;
-        const HEADER_RETRACTION_DISTANCE: number = HEADER_REVEALED_HEIGHT - HEADER_RETRACTED_HEIGHT;
+    const {config = defaultConfig} = props;
+    const {
+      HEADER_RETRACTED_HEIGHT = DEFAULT_HEADER_RETRACTED_HEIGHT,
+      HEADER_REVEALED_HEIGHT = DEFAULT_HEADER_REVEALED_HEIGHT,
+    } = config.header;
+    const HEADER_RETRACTION_DISTANCE: number =
+      HEADER_REVEALED_HEIGHT - HEADER_RETRACTED_HEIGHT;
 
-        this.scrollY = new Animated.Value(HEADER_RETRACTION_DISTANCE);
-    }
+    this.scrollY = new Animated.Value(HEADER_RETRACTION_DISTANCE);
+  }
 
-    private readonly onOrientationChange = () => {
-        this.props.updateOrientation(isPortrait() ? 'portrait' : 'landscape');
-    };
+  private readonly onOrientationChange = () => {
+    this.props.updateOrientation(isPortrait() ? 'portrait' : 'landscape');
+  };
 
-    componentDidMount(){
-        Dimensions.addEventListener('change', this.onOrientationChange);
-    }
-    
-    componentWillUnmount(){
-        Dimensions.removeEventListener('change', this.onOrientationChange);
-    }
+  componentDidMount() {
+    Dimensions.addEventListener('change', this.onOrientationChange);
+  }
 
-    render(){
-        const { config = defaultConfig } = this.props;
-        const { barAwareWebView = DefaultBarAwareWebView } = config;
-        // Visibility of certain components changes when switching app (if in private browsing mode)
-        // https://github.com/cliqz/user-agent-ios/blob/develop/Client/Frontend/Browser/BrowserViewController.swift#L343
+  componentWillUnmount() {
+    Dimensions.removeEventListener('change', this.onOrientationChange);
+  }
 
-        return (
-            <View
-                // stretchLastChild={true}
-                style={{
-                    flex: 1,
-                    flexDirection: "column",
-                    width: "100%",
-                    height: "100%",
-                }}
-            >
-                <RetractibleHeaderConnected
-                    config={config.header}
-                    scrollY={this.scrollY}
-                />
+  render() {
+    const {config = defaultConfig} = this.props;
+    const {barAwareWebView = DefaultBarAwareWebView} = config;
+    // Visibility of certain components changes when switching app (if in private browsing mode)
+    // https://github.com/cliqz/user-agent-ios/blob/develop/Client/Frontend/Browser/BrowserViewController.swift#L343
 
-                <View
-                    style={{
-                        flex: 1,
-                        width: "100%",
-                        height: "100%",
-                        flexGrow: 1,
-                        alignItems:"center",
-                        backgroundColor:"green",
-                        flexDirection: "column",
-                    }}
-                >
-                    <View
-                        style={{
-                            flex: 1,
-                            flexDirection: "column",
-                            width: "100%",
-                        }}
-                    >
-                        <WebViewBackdrop
-                            style={{
-                                backgroundColor: "gold",
-                                position: "absolute",
-                            }}
-                        />
-                        {barAwareWebView({
-                            headerConfig: config.header,
-                            scrollY: this.scrollY,
-                            scrollEndDragVelocity: this.scrollEndDragVelocity,
-                        })}
-                    </View>
+    return (
+      <View
+        // stretchLastChild={true}
+        style={{
+          flex: 1,
+          flexDirection: 'column',
+          width: '100%',
+          height: '100%',
+        }}>
+        <RetractibleHeaderConnected
+          config={config.header}
+          scrollY={this.scrollY}
+        />
 
-                    <FooterConnected
-                        config={config.footer}
-                        scrollY={this.scrollY}
-                        showToolbar={true}
-                    />
-                </View>
-            </View>
-        );
-    }
+        <View
+          style={{
+            flex: 1,
+            width: '100%',
+            height: '100%',
+            flexGrow: 1,
+            alignItems: 'center',
+            backgroundColor: 'green',
+            flexDirection: 'column',
+          }}>
+          <View
+            style={{
+              flex: 1,
+              flexDirection: 'column',
+              width: '100%',
+            }}>
+            <WebViewBackdrop
+              style={{
+                backgroundColor: 'gold',
+                position: 'absolute',
+              }}
+            />
+            {barAwareWebView({
+              headerConfig: config.header,
+              scrollY: this.scrollY,
+              scrollEndDragVelocity: this.scrollEndDragVelocity,
+            })}
+          </View>
+
+          <FooterConnected
+            config={config.footer}
+            scrollY={this.scrollY}
+            showToolbar={true}
+          />
+        </View>
+      </View>
+    );
+  }
 }
 
 export const BrowserViewControllerConnected = connect(
-    (wholeStoreState: WholeStoreState) => {
-        // console.log(`wholeStoreState`, wholeStoreState);
-        return {};
-    },
-    {
-        updateOrientation: updateOrientation
-    },
+  (wholeStoreState: WholeStoreState) => {
+    // console.log(`wholeStoreState`, wholeStoreState);
+    return {};
+  },
+  {
+    updateOrientation: updateOrientation,
+  },
 )(BrowserViewController);
