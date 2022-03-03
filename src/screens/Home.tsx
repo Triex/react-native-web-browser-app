8f94d772affb1666309b603270d6a7b1faf3042b
Alex Zarov <alex@imakewebsites.co>
2022-03-03 17:00:32 +1100

    Home, gradientprogbar, privacyindicatorview, retractibleheader.js/tsx, navigationState.js

---
+++ src/screens/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'React';
+import {View, Text} from 'react-native';
+
+const Home = () => {
+  return (
+    <View>
+      <Text>This is Home</Text>
+    </View>
+  );
+};
+
+export default Home;
